3557e114f3b25161cdc78ed841ce825667b4b6ce
Loyer Guillaume <loyer.g@gmail.com>
2016-10-05 21:27:03 +0200

    Asset Graphics - Invisible load mesh

--- ts/utils/game/AssetGraphic.ts
+++ ts/utils/game/AssetGraphic.ts
@@ -18,14 +18,23 @@ class AssetGraphic extends GameObject {
     }
 
     public static addObject(id: string, pMeshes: BABYLON.Mesh[] = [], pSkeletons : any[] = [], pParticleSystems: any[] = []) {
+        pMeshes = AssetGraphic.invisibleAllMesh(pMeshes);
         AssetGraphic.meshesList[id] = pMeshes;
         AssetGraphic.skeletonsList[id] = pSkeletons;
         AssetGraphic.particlesSystemsList[id] = pParticleSystems;
     }
 
+    private static invisibleAllMesh(pMeshes:BABYLON.Mesh[]): BABYLON.Mesh[] {
+        for (var i = 0; i < pMeshes.length; i++) {
+            pMeshes[i].isVisible = false;
+        }
+
+        return pMeshes;
+    }
+
     private setAsset(pAssetName: string, pScene: BABYLON.Scene): void {
         this.assetName = pAssetName;
-        
+
         this.meshes = AssetGraphic.meshesList[pAssetName] || [BABYLON.Mesh.CreateBox(pAssetName, 1, pScene)];
         this.skeletons = AssetGraphic.skeletonsList[pAssetName] || [];
         this.particleSystems = AssetGraphic.particlesSystemsList[pAssetName] || [];

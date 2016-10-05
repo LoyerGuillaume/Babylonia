3b2c4877681a07be6d30034b117130e961d98041
Pierre <p.machkovsky@student.isartdigital.com>
2016-10-05 22:34:01 +0200

    update cameraManager

--- ts/CameraManager.ts
+++ ts/CameraManager.ts
@@ -2,16 +2,21 @@ class CameraManager {
 
     private static camera:BABYLON.FollowCamera;
 
+    private static get RADIUS() { return 800;};
+    private static get HEIGHT() { return 500;};
+    private static get ANGLE() { return 120;};
+
 
     public static init(pScene: BABYLON.Scene, pEngine: BABYLON.Engine) {
         CameraManager.camera = new BABYLON.FollowCamera('FollowCamera', new BABYLON.Vector3(0, 0, 0), pScene);
-
-        // pScene.activeCamera = camera;
     }
 
 
     public static setTarget(pMesh: BABYLON.AbstractMesh) {
         CameraManager.camera.target = pMesh;
+        CameraManager.camera.radius = CameraManager.RADIUS;
+        CameraManager.camera.heightOffset = CameraManager.HEIGHT;
+        CameraManager.camera.rotationOffset = CameraManager.ANGLE;
     }
 
 

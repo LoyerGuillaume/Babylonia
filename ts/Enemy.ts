f5278cef472fbe68c41da0437d040da0d09f2aed
Loyer Guillaume <loyer.g@gmail.com>
2016-10-16 19:28:09 +0200

    Enemy - Better Rotation

--- ts/Enemy.ts
+++ ts/Enemy.ts
@@ -24,18 +24,20 @@ class Enemy extends AssetGraphic {
         var target = Player.list[0];
         var vectorMovement:BABYLON.Vector3 = target.position.subtract(this.position);
 
-        // var vectorMovement:BABYLON.Vector3 = new BABYLON.Vector3(this.control ler.horizontal, 0, this.controller.vertical);
-        var vectorMovementNormalized:BABYLON.Vector3 = vectorMovement.normalize();
-        // console.log(vectorMovementNormalized);
+        vectorMovement.normalize();
 
         this.moveWithCollisions(vectorMovement.scaleInPlace(Enemy.MOVE_SPEED * deltaTime));
 
-        var rotation = BABYLON.Tools.ToDegrees(Math.atan2(vectorMovementNormalized.y, vectorMovementNormalized.x));
-        // console.log(rotation);
-        rotation -= 90;
-        var q = BABYLON.Quaternion.RotationAxis(BABYLON.Vector3.Up(), BABYLON.Tools.ToRadians(rotation));
-        this.rotationQuaternion = BABYLON.Quaternion.Slerp(this.rotationQuaternion.clone(), q, Enemy.ROTATION_SPEED);
+        this._rotate(vectorMovement);
+    }
 
+
+    private _rotate (vectorMovement:BABYLON.Vector3) {
+        vectorMovement = vectorMovement.multiplyByFloats(-1, -1, -1);
+        var rotation = BABYLON.Tools.ToDegrees(Math.atan2(vectorMovement.z, vectorMovement.x));
+        rotation -= 90;
+        var q = BABYLON.Quaternion.RotationAxis(BABYLON.Vector3.Up(), BABYLON.Tools.ToRadians(-rotation));
+        this.rotationQuaternion = BABYLON.Quaternion.Slerp(this.rotationQuaternion.clone(), q, Enemy.ROTATION_SPEED);
     }
 
 

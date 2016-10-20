294541c93e28968b39315447c34cdf5b5bf0be7f
Loyer Guillaume <loyer.g@gmail.com>
2016-10-20 14:23:21 +0200

    Score - compute score in variable

--- ts/events/PlayerEvent.ts
+++ ts/events/PlayerEvent.ts
@@ -4,9 +4,11 @@ class PlayerEvent extends BEvent {
     public static HAS_HIT:string = 'PLAYER_HAS_HIT';
 
     public player:Player;
+    public enemyScore:number;
 
-    constructor(pPlayer:Player, pEventName:string) {
+    constructor(pEventName:string, pPlayer:Player, pEnemyScore:number = 0) {
         super(pEventName);
-        this.player = pPlayer;
+        this.player     = pPlayer;
+        this.enemyScore = pEnemyScore;
     }
 }

584f9cfa496427f7d240ac33af7a055df1fadaec
Dimitri <d.pomier@student.isartdigital.com>
2016-10-22 20:49:26 +0200

    event once

--- ts/utils/events/BEvent.ts
+++ ts/utils/events/BEvent.ts
@@ -1,20 +1,32 @@
 class BEvent {
 
     private static callbacks:any = {};
+    private static onces:any     = {};
 
     public id:string;
 
     private params:any;
+
 
     constructor (pId:string, pParams:any = undefined) {
         this.id = pId;
         this.params = pParams;
     }
 
+    public static once (pId:string, pFunc:any, pThis:any) {
+
+        if (!BEvent.onces[pId]) {
+            BEvent.onces[pId] = [];
+        }
+
+        BEvent.onces[pId].push(pFunc);
+        BEvent.onces[pId].push(pThis);
+    }
+
     public static on (pId:string, pFunc:any, pThis:any) {
 
         if (!BEvent.callbacks[pId]) {
-            BEvent.callbacks[pId] = []
+            BEvent.callbacks[pId] = [];
         }
 
         BEvent.callbacks[pId].push(pFunc);
@@ -22,6 +34,8 @@ class BEvent {
     }
 
     public static off (pId:string, pFunc:any, pThis:any) {
+
+        console.warn('want delete ', pId, pFunc, pThis);
 
         if (BEvent.callbacks[pId]) {
 
@@ -31,6 +45,7 @@ class BEvent {
                 if (BEvent.callbacks[pId][i+1] === pThis) {
 
                     if (pThis !== undefined && BEvent.callbacks[pId][i].name === pFunc.name || BEvent.callbacks[pId][i].toString() === pFunc.toString()) {
+                        console.warn('delete of ', pId, pFunc, pThis);
                         BEvent.callbacks[pId].splice(i, 2);
                         break;
                     }
@@ -47,6 +62,14 @@ class BEvent {
             var len:number = BEvent.callbacks[pEvent.id].length;
             for (var i = 0; i < len; i+=2) {
                 BEvent.callbacks[pEvent.id][i].call(BEvent.callbacks[pEvent.id][i+1], pEvent.params);
+            }
+        }
+
+        if (BEvent.onces[pEvent.id]) {
+            var len:number = BEvent.onces[pEvent.id].length;
+            for (var i = 0; i < len; i+=2) {
+                BEvent.onces[pEvent.id][0].call(BEvent.onces[pEvent.id][1], pEvent.params);
+                BEvent.callbacks[pEvent.id].splice(0, 2);
             }
         }
     }

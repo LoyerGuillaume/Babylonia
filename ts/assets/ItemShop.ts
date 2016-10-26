d2c9470f221d523ea28064f5648d7af94785fb28
Loyer Guillaume <loyer.g@gmail.com>
2016-10-26 23:28:23 +0200

    ShopManager - Update

--- ts/assets/ItemShop.ts
+++ ts/assets/ItemShop.ts
@@ -3,15 +3,32 @@ class ItemShop extends AssetGraphic {
     public static list:ItemShop[] = [];
 
     private startYPosition:number;
+    private cost:number;
+    private bonusCallback;
 
-    constructor(pScene:BABYLON.Scene, pAssetName:string, pPosition:BABYLON.Vector3) {
+    constructor(pScene:BABYLON.Scene, pAssetName:string, pCost:number, pCallback) {
         super(pAssetName, pScene);
 
-        this.position           = pPosition.clone();
-        this.startYPosition     = this.position.y;
+        this.cost = pCost;
+        this.bonusCallback = pCallback;
         this.rotationQuaternion = BABYLON.Quaternion.RotationAxis(BABYLON.Vector3.Up(), 0);
 
-        console.log(this.position);
         ItemShop.list.push(this);
+    }
+
+    public setPosition (pPosition:BABYLON.Vector3):void {
+        this.position = pPosition.clone();
+        this.startYPosition     = this.position.y;
+    }
+
+
+
+    private checkPlayerCollision () :void {
+        for (var i in Player.list) {
+            if (Tools.minusVector3(this.position, Player.list[i].position).length() < 0.8) {
+                this.bonusCallback(Player.list[i]);
+                return;
+            }
+        }
     }
 }
--- ts/managers/ShopManager.ts
+++ ts/managers/ShopManager.ts
@@ -3,6 +3,8 @@ class ShopManager {
 
     private static PEDESTRAL_NAME: string = 'pedestral';
 
+    private itemShopList:ItemShop[];
+
     private pedestralPos:BABYLON.Vector3[];
     private mainScene:BABYLON.Scene;
 
@@ -13,11 +15,25 @@ class ShopManager {
         this.popItem();
     }
 
+    private initItemShopList ():void {
+        this.itemShopList = [];
+
+        this.itemShopList[0] = new ItemShop(this.mainScene, 'Bottle_Health', 10, this.addHealth);
+        this.itemShopList[1] = new ItemShop(this.mainScene, 'Bottle_Health', 10, this.addHealth);
+        this.itemShopList[2] = new ItemShop(this.mainScene, 'Bottle_Health', 10, this.addHealth);
+
+    }
+
+    private addHealth (pPlayer:Player):void {
+        console.log('addHealth');
+    }
+
 
 
     private initPositionItemShop (pLevelManager:LevelManager) {
         this.pedestralPos = [];
         for (var i = 1; i <= 3; i++) {
+            console.log(pLevelManager.getGameplayObjectUnique(ShopManager.PEDESTRAL_NAME + '0' + i));
             this.pedestralPos.push(pLevelManager.getGameplayObjectUnique(ShopManager.PEDESTRAL_NAME + '0' + i).mesh.position.clone());
         }
 
@@ -26,12 +42,12 @@ class ShopManager {
 
     private popItem () {
         var addPosition:BABYLON.Vector3 = new BABYLON.Vector3(0, 1, 0);
-        var itemShop1 = new ItemShop(this.mainScene, 'Bottle_Health', this.pedestralPos[0].clone().add(addPosition));
-        itemShop1.start();
-        var itemShop2 = new ItemShop(this.mainScene, 'Bottle_Health', this.pedestralPos[1].clone().add(addPosition));
-        itemShop2.start();
-        var itemShop3 = new ItemShop(this.mainScene, 'Bottle_Health', this.pedestralPos[2].clone().add(addPosition));
-        itemShop3.start();
+        this.itemShopList[0].setPosition(this.pedestralPos[0].clone().add(addPosition));
+        this.itemShopList[0].start();
+        this.itemShopList[1].setPosition(this.pedestralPos[1].clone().add(addPosition));
+        this.itemShopList[1].start();
+        this.itemShopList[2].setPosition(this.pedestralPos[2].clone().add(addPosition));
+        this.itemShopList[2].start();
     }
 
 }

82e7ab194f1f0370877fbaa8b97f8bb4b432a9f7
Dimitri <d.pomier@student.isartdigital.com>
2016-10-27 02:35:13 +0200

    knife hurtle new settings

--- ts/assets/attacks/KnifeHurtle.ts
+++ ts/assets/attacks/KnifeHurtle.ts
@@ -3,10 +3,14 @@ class KnifeHurtle extends Ball {
     constructor(pScene:BABYLON.Scene, pPosition:BABYLON.Vector3, pRotation:BABYLON.Quaternion, pPlayer) {
         super('KnifeHurtle', pScene, pPosition, pRotation, pPlayer);
 
-        this.maxLifeTime    = 5;
+        this.maxLifeTime    = 4;
         this.speed          = 1;
         this.collisionRange = 1;
         this.damageDeal     = 1.5;
+    }
+
+    public onHit () {
+        // override for disable default behavior
     }
 
 }

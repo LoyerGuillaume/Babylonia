42e4eed3a356844942d543cd5a8aa524b3c95f0b
Pierre <p.machkovsky@student.isartdigital.com>
2016-10-28 21:05:24 +0200

    refactor UICapacity

--- ts/managers/UIManager.ts
+++ ts/managers/UIManager.ts
@@ -38,8 +38,6 @@ class UIManager {
 
         UIManager.heartTexture = Babylonia.getLoadedContent('heart.png', true);
         UIManager.heartScale = UIManager.HEART_SIZE / UIManager.heartTexture.getSize().width;
-
-        UICapacity.initTextures();
     }
 
     /**
@@ -48,7 +46,6 @@ class UIManager {
     public static loadTextures(scene:BABYLON.Scene, pCallback:any) {
         UIManager.heartTexture = new BABYLON.Texture('../assets/heart.png', scene, false, true, 0, function () {
             UIManager.heartScale = UIManager.HEART_SIZE / UIManager.heartTexture.getSize().width;
-            UICapacity.loadTextures(scene, pCallback);
         });
     }
 
